11a149cb6830c9093ff537caf754a8f4a7b09836
NATHAN-PC\Nathan <hobosanchez88@gmail.com>
2018-01-20 09:42:06 -0500

    hopefully i set it up right

---
+++ Game403GroupProject/Assets/__scripts/SceneManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+    public static SceneManager Instance = null;
+
+
+	// Use this for initialization
+	void Start () {
+		if(Instance = null)
+        {
+            Instance = this;
+        }
+        if(Instance != this)
+        {
+            Destroy(this);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //custom function go here
+
+}
