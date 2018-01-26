64ede636c8bcb9336f8eec09baf469224c357291
NATHAN-PC\Nathan <hobosanchez88@gmail.com>
2018-01-25 23:11:22 -0500

    added a demo game scene with a little racing car

---
+++ Game403GroupProject/Assets/__scripts/Game/Path.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour {
+    public Color linecolor;
+
+    private List<Transform> points = new List<Transform>();
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = linecolor;
+
+        Transform[] pointTransform = GetComponentsInChildren<Transform>();
+        points = new List<Transform>();
+        foreach(Transform t in pointTransform)
+        {
+            if(t != transform)
+            {
+                points.Add(t);
+            }
+        }
+
+        for (int i = 0; i < points.Count; i++)
+        {
+            Vector3 current = points[i].position;
+            Vector3 previous = Vector3.zero;
+            if (i > 0)
+            {
+                previous = points[i - 1].position;
+            }
+            else if (i == 0 && points.Count > 1)
+            {
+                previous = points[points.Count - 1].position;
+            }
+            Gizmos.DrawLine(previous, current);
+            Gizmos.DrawWireSphere(current, 1f);
+        }
+
+        
+    }
+}
