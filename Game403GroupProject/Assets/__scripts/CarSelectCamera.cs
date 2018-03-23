83e6b375d15c2f55d8754096692a4ef4901ff6df
SLCTECH\nlunagariya <nikunj.lunagariya@gmail.com>
2018-03-23 11:41:11 -0400

    MenuScene Change

---
+++ Game403GroupProject/Assets/__scripts/CarSelectCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSelectCamera : MonoBehaviour {
+
+	public float moveCam;
+
+	public float smooth;
+	// Use this for initialization
+	void Update () {
+		
+		if (Input.GetButtonDown("CamMove"))
+		{
+			transform.position = Vector3.Lerp (transform.position, target.position, Time.deltaTime * smooth);
+		}
+			
+	}
+}
