361674fe608a26a467b405195bf77b3a2a482896
alejandr2403 <alejandro.simon@cogeco.ca>
2018-03-28 08:32:22 -0400

    Tornado

---
+++ Game403GroupProject/Assets/__scripts/TornadoLogic.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TornadoLogic : MonoBehaviour {
+
+    public GameObject tornado;
+    public Vector3 spawnValues;
+    public int tornadoCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    void Start()
+    {
+        StartCoroutine (SpawnWaves());
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while(true)
+        {
+            for (int i = 0; i < tornadoCount; i ++)
+            {
+                Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, Random.Range(-spawnValues.z, spawnValues.z));
+                Quaternion spawnRotation = new Quaternion();
+                Instantiate(tornado, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
+}
