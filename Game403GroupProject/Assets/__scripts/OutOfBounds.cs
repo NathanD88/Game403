bd58d650519a992af43b0d54a0c4530a5790c36e
Matthew Brunton <matthewagbrunton@gmail.com>
2018-04-08 16:08:29 -0400

    In Progress

---
+++ Game403GroupProject/Assets/__scripts/OutOfBounds.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Car thisCar;
+        if (other.CompareTag("PlayerCar") && (thisCar = other.GetComponentInParent<Car>()))
+        {
+            thisCar.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            thisCar.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            thisCar.transform.rotation = thisCar.resetView;
+            thisCar.transform.position = thisCar.resetPosition;
+            //thisCar.GetComponent<Rigidbody>().rotation = thisCar.resetView;
+            
+        }
+    }
+}
