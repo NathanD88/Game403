251ddf50b885e614184b62df24427b06b5614af4
Matthew Brunton <matthewagbrunton@gmail.com>
2018-04-19 21:29:51 -0400

    [TS-102][TS-103] Continued Work

--- Game403GroupProject/Assets/__scripts/Game/MobileHUDController.cs
+++ Game403GroupProject/Assets/__scripts/Game/MobileHUDController.cs
@@ -7,12 +7,24 @@ public class MobileHUDController : MonoBehaviour, IPointerUpHandler, IPointerDow
 {
     private bool pressed;
     private RVP.MobileInput mobileInput;
+    private Car playerCar;
+
+    [System.Serializable]
+    public enum buttonType
+    {
+        Acceleration,
+        Brake,
+        Fire
+    };
+
+    public buttonType buttonControl;
 
 	// Use this for initialization
 	void Start ()
     {
         pressed = false;
         mobileInput = FindObjectOfType<RVP.MobileInput>();
+        playerCar = FindObjectOfType<RVP.CameraControl>().target.gameObject.GetComponent<Car>();
 	}
 	
 	// Update is called once per frame
@@ -20,11 +32,29 @@ public class MobileHUDController : MonoBehaviour, IPointerUpHandler, IPointerDow
     {
 		if (pressed)
         {
-            mobileInput.SetAccel(1.0f);
+            if (buttonControl == buttonType.Acceleration)
+            {
+                mobileInput.SetAccel(1.0f);
+            }
+            if (buttonControl == buttonType.Brake)
+            {
+                mobileInput.SetBrake(1.0f);
+            }
+            if (buttonControl == buttonType.Fire)
+            {
+                playerCar.checkFire();
+            }
         }
         else
         {
-            mobileInput.SetAccel(0.0f);
+            if (buttonControl == buttonType.Acceleration)
+            {
+                mobileInput.SetAccel(0.0f);
+            }
+            if (buttonControl == buttonType.Brake)
+            {
+                mobileInput.SetBrake(0.0f);
+            }
         }
 	}
 
